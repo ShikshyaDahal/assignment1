59a18b65e734ad05222db1bb4052c8961be5aaef
ShikshyaDahal <st120186@ait.asia>
2019-02-03 13:35:44 +0700

    MVC architecture implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :subjects
+  resources :courses
+
+  root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
