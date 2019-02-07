63cc4d2ae4fcf8f4a1139d1f2721ea98f087e4c6
developeravsk <readeravskh@gmail.com>
2019-02-07 13:19:22 +0700

    Minor Bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :subjects
-  resources :courses
+   resources :subjects
+  resources :courses do
+    resource :subjects
+  end
 
   root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
